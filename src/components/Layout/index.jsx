c978f9bb664a671beba92fc0891bc3712dd317e6
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-05-25 17:52:32 +0200

    feat: add cloudflare integration

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import '../../assets/scss/init.scss'
 import ForkMeOnGithub from '../ForkMeOnGithub/index'
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 
 class Layout extends React.Component {
   render() {
     const { children } = this.props
+
+    getVisitorId()
 
     return (
       <>
@@ -26,6 +29,22 @@ class Layout extends React.Component {
       </>
     )
   }
+}
+
+function getVisitorId() {
+  console.log("getting visitorId");
+  // Initialize an agent at application startup.
+  const fpPromise = FingerprintJS.load({
+    apiKey: 'tQUwQQOuG9TNwqc6F4I2', // REPLACE <API_KEY>
+    scriptUrlPattern:
+      '/fpjs-pro-worker/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>', // ADDED (as is)
+    endpoint: '/fpjs-pro-worker/visitorId', // CHANGED
+  })
+
+  // Get the visitor identifier when you need it.
+  fpPromise
+    .then((fp) => fp.get())
+    .then((result) => console.log(result.visitorId))
 }
 
 export default Layout
