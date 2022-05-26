d72d6af5a65b8ffd2ff333c950aab366f85d95f6
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-05-26 10:43:04 +0200

    fix: fpjs region

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -35,11 +35,10 @@ function getVisitorId() {
   console.log("getting visitorId");
   // Initialize an agent at application startup.
   const fpPromise = FingerprintJS.load({
-    apiKey: 'tQUwQQOuG9TNwqc6F4I2', // REPLACE <API_KEY>
+    apiKey: 'tQUwQQOuG9TNwqc6F4I2', // REPLACE <API_KEY>,
     scriptUrlPattern:
-      '/fpjs-pro-worker/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>', // ADDED (as is)
+      '/fpjs-pro-worker/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>&region=eu', // ADDED (as is)
     endpoint: '/fpjs-pro-worker/visitorId', // CHANGED
-    region: 'eu'
   })
 
   // Get the visitor identifier when you need it.
